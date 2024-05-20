1f69b7e174d01a186e41d37d7aa843915495b5d3
lievin219 <gakizalievin219@gmail.com>
2024-05-20 15:57:33 +0200

    posted well done succesfully

--- server/src/data.js
+++ server/src/data.js
@@ -1,17 +1,13 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  user: "pern_project_vo7v_user",
-    password: "4mUWgF0T4zE2WmyzFEQ3DLLfAdiyhvjy",
-    host: "dpg-cp5gvs0cmk4c73f1vp0g-a",
+  
+   
+    user: "postgres",
+    password: "gakiza2004",
+    host: "localhost",
     port: 5432,
-    database: "pern_project_vo7v"
-    // user: "postgres",
-    // password: "gakiza2004",
-    // host: "localhost",
-    // port: 5432,
-    // database: "pernproject" // Should be lowercase "database", not "DATABASE"
+    database: "pernproject" // Should be lowercase "database", not "DATABASE"
 });
 pool.connect((err, client, release) => {
     if (err) {
